9914e9f68ac75838779cfe27f8d41a09079ab2bb
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-05-03 12:51:25 +0200

    update deps

--- dist/assets/js/deps.mjs
+++ dist/assets/js/deps.mjs
@@ -1,6 +1,6 @@
 // The application's JavaScript dependencies.
 
-export { QworumScript, Qworum } from "https://esm.sh/gh/doga/qworum-for-web-pages@1.6.8/mod.mjs";
+export { QworumScript, Qworum } from "https://esm.sh/gh/doga/qworum-for-web-pages@1.7.0/mod.mjs";
 
 import dataFactory from 'https://esm.sh/gh/rdfjs-base/data-model@v2.0.1/index.js';
 export { dataFactory };
